85c8721d2f5565db32a704ab581bd9be9b690fc1
Jose Maria Roman <jose.maria.roman87@gmail.com>
2024-04-23 17:43:40 +0200

    Initial commit

---
+++ lib/open_weather_aemet.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "open_weather_aemet/version"
+
+module OpenWeatherAemet
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/open_weather_aemet/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module OpenWeatherAemet
+  VERSION = "0.1.0"
+end
